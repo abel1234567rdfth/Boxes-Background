35b97649f32fdfffddc40a09ae36a94aad5b1314
Abel Guta <gutaabel546@gmail.com>
2025-11-01 21:34:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+
+function createBoxes(){
+    for(let i = 0;i<4;i++){
+        for (let j = 0; j < 4; j++) {
+           const boxEl = document.createElement('div')
+           boxEl.classList.add('box')
+           boxEl.style.backgroundPosition = `${-j * 125}px ${-i*125}px`
+           boxesContainer.appendChild(boxEl)
+            
+        }
+
+    }
+}
+
+btn.addEventListener('click',()=>{
+    boxesContainer.classList.toggle('big')
+})
+
+createBoxes()
